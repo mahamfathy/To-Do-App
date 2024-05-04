ebccd66534dbe24c0dc4744106a4457dbaeb5dea
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-05 00:57:07 +0300

    handling the routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    // or we can add withfetch function
+    // provideHttpClient(withFetch())
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent, title: 'Home' },
+  { path: 'todos', component: TodosComponent, title: 'ToDos' },
+  { path: 'todos/:id', component: TodoDetailsComponent, title: 'To Do Details' },
+  { path: 'contact-us', component: ContactUsComponent, title: 'Contact Us' },
+  { path: 'about-us', component: AboutUsComponent, title: 'About Us' },
+  { path: '**', component: NotFoundComponent, title: 'Not Found' },
+
+];
