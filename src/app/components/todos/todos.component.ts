2d4c86348be54eda0525d20e131e5de873639742
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-05-05 20:09:08 +0300

    create to do function

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToDo } from '../../Models/to-do';
+import { ToDoService } from '../../Services/to-do.service';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
-export class TodosComponent {
-
+export class TodosComponent implements OnInit {
+  todos: ToDo[] = [];
+  newTodo: ToDo = {} as ToDo;
+  constructor(private todoService: ToDoService) {}
+  ngOnInit(): void {
+    this.getTodos();
+  }
+  getTodos() {
+    this.todoService.getTodos().subscribe((allTodos) => {
+      this.todos = allTodos;
+    });
+  }
+  createTodo():void {
+    const newTodo1 = {id:this.newTodo.id,title:this.newTodo.title,completed:false}
+    this.newTodo = newTodo1
+    this.todoService.createToDo(newTodo1).subscribe(todo=>{
+      this.todos.push(todo)
+    })
+  }
 }
